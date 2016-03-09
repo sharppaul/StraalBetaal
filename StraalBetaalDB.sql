d6b0ba1b63825edf541e640ba7b6ac102bf04a98
Yoshua <yosh.nahar@gmail.com>
2016-03-10 00:57:58 +0100

    Update StraalBetaalDB.sql

--- StraalBetaalDB.sql
+++ StraalBetaalDB.sql
@@ -1,21 +1,22 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server versie:                5.7.9-log - MySQL Community Server (GPL)
+-- Server version:               5.7.10-log - MySQL Community Server (GPL)
 -- Server OS:                    Win64
--- HeidiSQL Versie:              9.3.0.5009
+-- HeidiSQL Version:             9.3.0.5036
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Databasestructuur van straalbetaal wordt geschreven
+-- Dumping database structure for straalbetaal
 CREATE DATABASE IF NOT EXISTS `straalbetaal` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `straalbetaal`;
 
 
--- Structuur van  tabel straalbetaal.betaalgeschiedenis wordt geschreven
+-- Dumping structure for table straalbetaal.betaalgeschiedenis
 CREATE TABLE IF NOT EXISTS `betaalgeschiedenis` (
   `cardID` char(50) NOT NULL,
   `af_bij_geschreven` int(11) DEFAULT NULL,
@@ -23,10 +24,13 @@ CREATE TABLE IF NOT EXISTS `betaalgeschiedenis` (
   PRIMARY KEY (`cardID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporteren was gedeselecteerd
+-- Dumping data for table straalbetaal.betaalgeschiedenis: ~0 rows (approximately)
+DELETE FROM `betaalgeschiedenis`;
+/*!40000 ALTER TABLE `betaalgeschiedenis` DISABLE KEYS */;
+/*!40000 ALTER TABLE `betaalgeschiedenis` ENABLE KEYS */;
 
 
--- Structuur van  tabel straalbetaal.cards wordt geschreven
+-- Dumping structure for table straalbetaal.cards
 CREATE TABLE IF NOT EXISTS `cards` (
   `cardID` char(50) NOT NULL,
   `userID` char(50) NOT NULL,
@@ -34,20 +38,33 @@ CREATE TABLE IF NOT EXISTS `cards` (
   PRIMARY KEY (`cardID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporteren was gedeselecteerd
+-- Dumping data for table straalbetaal.cards: ~2 rows (approximately)
+DELETE FROM `cards`;
+/*!40000 ALTER TABLE `cards` DISABLE KEYS */;
+INSERT INTO `cards` (`cardID`, `userID`, `pincode`) VALUES
+	('12345', 'JasonP', '3025'),
+	('9999', 'GarePaul', '4200');
+/*!40000 ALTER TABLE `cards` ENABLE KEYS */;
 
 
--- Structuur van  tabel straalbetaal.saldo wordt geschreven
+-- Dumping structure for table straalbetaal.saldo
 CREATE TABLE IF NOT EXISTS `saldo` (
   `cardID` char(50) NOT NULL,
   `cardSaldo` int(11) NOT NULL,
   PRIMARY KEY (`cardID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporteren was gedeselecteerd
+-- Dumping data for table straalbetaal.saldo: ~3 rows (approximately)
+DELETE FROM `saldo`;
+/*!40000 ALTER TABLE `saldo` DISABLE KEYS */;
+INSERT INTO `saldo` (`cardID`, `cardSaldo`) VALUES
+	('12345', 1000),
+	('55551', 0),
+	('9999', 50);
+/*!40000 ALTER TABLE `saldo` ENABLE KEYS */;
 
 
--- Structuur van  tabel straalbetaal.users wordt geschreven
+-- Dumping structure for table straalbetaal.users
 CREATE TABLE IF NOT EXISTS `users` (
   `userID` char(50) NOT NULL,
   `firstname` varchar(50) NOT NULL,
@@ -57,7 +74,12 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`userID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporteren was gedeselecteerd
+-- Dumping data for table straalbetaal.users: ~1 rows (approximately)
+DELETE FROM `users`;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`userID`, `firstname`, `lastname`, `email`, `registeredDate`) VALUES
+	('JasonP', 'Jason', 'Phillips', 'stupidnerd69@gmail.com', '2016-03-09 18:30:26');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
