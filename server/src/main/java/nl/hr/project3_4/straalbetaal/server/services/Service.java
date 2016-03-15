70d699d6b62668e6279b3d317b2abfd76f81a111
Yoshua <yosh.nahar@gmail.com>
2016-03-15 16:38:19 +0100

    Update Service.java

--- server/src/main/java/nl/hr/project3_4/straalbetaal/server/services/Service.java
+++ server/src/main/java/nl/hr/project3_4/straalbetaal/server/services/Service.java
@@ -6,11 +6,13 @@ import nl.hr.project3_4.straalbetaal.server.dao.DataAccessObject;
 public class Service {
 
 	private static DataAccessObject dao = new DataAccessObject();
-	private boolean pincodeChecked = true; // (Testing with true for now) -> Mostlikely this whole class is instantiated every time a request is sent, so it will stay on false.
+	private boolean pincodeChecked; // (Testing with true for now) -> Mostlikely this whole class is instantiated every time a request is sent, so it will stay on false.
 	private long balance = 0;
 
 
-	public Service() {}
+	public Service() {
+		pincodeChecked = false;
+	}
 
 
 	public String getUserID(String iban, long pincode) {
