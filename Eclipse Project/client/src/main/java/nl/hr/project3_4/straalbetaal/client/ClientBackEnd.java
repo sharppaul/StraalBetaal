8eba648086775fb80ba9bfcb611cff5f3d7ce58d
sharppaul <weerheim.paul@gmail.com>
2016-06-02 23:35:09 +0200

    Cleaned up client backend, fixed wrong IP, compiled a new JAR.

--- Eclipse Project/client/src/main/java/nl/hr/project3_4/straalbetaal/client/ClientBackEnd.java
+++ Eclipse Project/client/src/main/java/nl/hr/project3_4/straalbetaal/client/ClientBackEnd.java
@@ -14,21 +14,16 @@ import nl.hr.project3_4.straalbetaal.api.*;
 public class ClientBackEnd {
 
 	private static final Logger LOG = Logger.getLogger(ClientBackEnd.class.getName());
-	// private SSLContext s;
 	private ClientBuilder cBuilder = ClientBuilder.newBuilder();
 	private Client client;
 
-	private static final String TARGET = "http://localhost:8025";
-	// private static final String TARGET = "http://145.24.222.208:8025";
+	private static final String TARGET = "http://145.24.222.208:8025";
 
-	// IP JASPER:
 	// private static final String TARGET = "http://145.24.222.211:8025";
-
-	/*
-	 * I did it this way - get the IBAN and pincode on initialization - , and
-	 * not get the IBAN and pincode per method, because that is how we set our
-	 * arduino code to also send the data to the client! (06-03-2016)
-	 */
+	// //Jasper or GetBenktM8
+	
+	// private static final String TARGET = "http://localhost:8025";
+	// //Localhost..
 
 	public ClientBackEnd() {
 		cBuilder.register(JacksonFeature.class);
@@ -38,41 +33,39 @@ public class ClientBackEnd {
 	public CheckPasResponse checkPas(CheckPasRequest request) {
 		String path = "/checkpas";
 
-		LOG.info("Client - CheckPincode Response send to server!");
+		LOG.info("Client - CheckPasRequest send to server!");
 		CheckPasResponse response = client.target(TARGET).path(path).request()
 				.post(Entity.entity(request, MediaType.APPLICATION_JSON), CheckPasResponse.class);
+		LOG.info("Client - CheckPasResponse received from server!");
 		return response;
 	}
 
 	public CheckPinResponse checkPincode(CheckPinRequest request) {
 		String path = "/checkpin";
 
-		LOG.info("Client - CheckPincode Response send to server!");
+		LOG.info("Client - CheckPincodeRequest send to server!");
 		CheckPinResponse response = client.target(TARGET).path(path).request()
 				.post(Entity.entity(request, MediaType.APPLICATION_JSON), CheckPinResponse.class);
+		LOG.info("Client - CheckPincodeResponse received from server!");
 		return response;
 	}
 
 	public BalanceResponse checkBalance(BalanceRequest request) {
 		String path = "/balance";
 
-		LOG.info("Client - Balance Response send to server!");
+		LOG.info("Client - BalanceRequest send to server!");
 		BalanceResponse response = client.target(TARGET).path(path).request()
 				.post(Entity.entity(request, MediaType.APPLICATION_JSON), BalanceResponse.class);
+		LOG.info("Client - BalanceResponse received from server!");
 		return response;
 	}
 
-	// Not tested!
 	public WithdrawResponse withdrawMoney(WithdrawRequest request) {
 		String path = "/withdraw";
-		LOG.info("Client - Withdraw Request send to server!");
+		LOG.info("Client - WithdrawRequest send to server!");
 		WithdrawResponse response = client.target(TARGET).path(path).request()
 				.post(Entity.entity(request, MediaType.APPLICATION_JSON), WithdrawResponse.class);
-		if (!response.isSucceeded()) {
-			// response.setResponse("Pinnen is helaas mislukt. Hebt u voldoende
-			// saldo?");
-		}
-		LOG.info("Client - Withdraw response received from server!");
+		LOG.info("Client - WithdrawResponse received from server!");
 		return response;
 	}
 
